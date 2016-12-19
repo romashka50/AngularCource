17a4f6c9b9f5ed4449686f4ab8205e38feb286b5
romashka50 <roma.buchuk@gmail.com>
2016-12-19 18:19:58 +0200

    setTimeOut

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,7 @@ var app = angular.module('myModule', []);
 
 app.controller('TestController', function($scope){
     $scope.myVar = 10;
-    $scope.myVar2 = 100;
+    setTimeout(function(){
+        $scope.myVar2 = 10;
+    }, 1000);
 });
