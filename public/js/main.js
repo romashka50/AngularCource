25ad33292a262356d758638b967b30e6f40174cb
romashka50 <roma.buchuk@gmail.com>
2016-12-19 18:08:21 +0200

    badlawcode

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,6 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myModule', []);
 
-app.controller('testCtrl', function ($scope) {
-    $scope.items = [...];
+app.controller('TestController', function($scope){
+    $scope.myVar = 10;
+    $scope.myVar2 = 100;
 });
-
-app.directive('listItem', function ($compile) {
-    return {
-        restrict: 'E',
-        replace : true,
-        scope   : {
-            item: '='
-        },
-        template: '<li></li>',
-        link    : function (scope, element) {
-            if (angular.isDefined(scope.item)) {
-                element.append('<h4>{{item.text}}</h4>');
-                $compile(element.contents())(scope)
-            }
-        }
-    }
-});
-
-app.directive('listManager', function () {
-    return {
-        restrict: 'E',
-        replace : true,
-        scope   : {
-            items: '='
-        },
-        template: '<ul><list-item ng-repeat="item in items track by $index" item="item"></list-item></ul>'
-    }
-});
-
