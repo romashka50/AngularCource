6dd8efdb4ae4df7934934c57f3f9ddb3b0e1cdce
romashka50 <roma.buchuk@gmail.com>
2016-12-23 19:25:18 +0200

    directives start

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,26 @@
 var app = angular.module('myModule', []);
 
-app.controller('TestController', function(){
-    this.myVar = 10;
-    this.myVar2 = 100;
-}).controller('Ctrl2', function($scope){
+app.controller('MyCtrl', function (myCustomFact) {
+    var self = this;
 
+    self.myVar = 10;
+    self.myVar2 = 100;
+
+    self.sum = function(){
+        return myCustomFact.sum(self.myVar, self.myVar2);
+    };
+}).directive('myCustomTag', function () {
+    return {
+        restrict    : 'E',
+        templateUrl : 'views/customDir.html',
+        controller  : 'MyCtrl',
+        controllerAs: 'myCtrl'
+    }
+}).factory('myCustomFact', function(){
+    return {
+        sum: function(a, b){
+            return a + b
+        }
+    }
 });
+
